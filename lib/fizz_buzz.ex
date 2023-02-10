0488e01467b4cd9b2b9f16b565fbd20921c39799
rodrigocitadin <citadinrodrigo@gmail.com>
2023-02-10 17:20:16 -0300

    Heigh-ho:
    We dig dig dig dig dig dig dig in our mine the
    whole day through
    To dig dig dig dig dig dig dig is what we really like to do
    It ain't no trick to get rich quick
    If you dig dig dig with a shovel or a pick
    In a mine! In a mine! In a mine! In a mine!
    Where a million diamonds shine!
    
    We dig dig dig dig dig dig dig from early morn till night
    We dig dig dig dig dig dig dig up everything in sight
    We dig up diamonds by the score
    A thousand rubies, sometimes more
    But we don't know what we dig 'em for
    We dig dig dig a-dig dig
    
    Heigh-ho, Heigh-ho
    Heigh-ho, Heigh-ho
    Heigh-ho
    
    [Chorus]
    Heigh-ho, Heigh-ho
    It's home from work we go
    [Whistle]
    
    Heigh-ho, Heigh-ho, Heigh-ho
    
    [Chorus]
    
    eigh-ho, Heigh-ho
    [Whistle]
    
    Heigh-ho, Heigh-ho
    Heigh-ho, Heigh-ho
    Heigh-ho, Heigh-ho
    Heigh-ho hum
    
    [Chorus three times]
    
    Heigh-ho [until fade]

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,14 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def call(list) when is_list(list) do
+    Enum.each(list, &fizzbuzz/1)
   end
+
+  defp fizzbuzz(number) when is_number(number) and number > 0 do
+    IO.puts(fizzbuzz(number, rem(number, 3), rem(number, 5)))
+  end
+
+  defp fizzbuzz(_number, 0, 0), do: "fizzbuzz"
+  defp fizzbuzz(_number, 0, _), do: "fizz"
+  defp fizzbuzz(_number, _, 0), do: "buzz"
+  defp fizzbuzz(number, _, _), do: number
 end
